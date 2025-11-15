5129f5bcc2d04aa15ed4e0174aea9f684daa93c6
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2025-11-14 23:24:00 -0300

    feat: adição da classe BaseEntity com colunas padrão

---
+++ src/resources/BaseEntity.resource.ts
@@ -0,0 +1,15 @@
+import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  declare id: string;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  declare created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  declare updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+  declare deleted_at?: Date | null;
+}
